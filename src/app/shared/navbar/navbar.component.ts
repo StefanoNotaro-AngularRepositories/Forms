5bdeaebdeeb7a1f5db410299a2bdd94cdbce8a88
Stefano Notaro <stefano.notaro@gmail.com>
2020-08-14 11:17:48 -0300

    BreadCrum and navbar improvements

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from './models/menu-item.model';
+import * as _ from 'underscore';
 
 @Component({
     selector: 'app-navbar',
@@ -8,18 +9,48 @@ import { MenuItem } from './models/menu-item.model';
 })
 export class NavbarComponent implements OnInit {
     public menues: MenuItem[] = [
-        { path: 'Home', displayName: 'Home', class: 'nav-item' },
-        { path: 'BootstrapInputsExamples', displayName: 'Bootstrap Inputs Examples', class: 'nav-item' },
+        { path: 'Home', displayName: 'Home', breadCrumPath: 'Home' },
         {
             path: '',
             displayName: 'Bootstrap Examples',
-            class: 'nav-item dropdown',
-            idDropDownList: true,
-            subMenues: [{ path: 'Login', displayName: 'Login', class: 'dropdown-item' }],
+            breadCrumPath: 'Bootstrap Examples',
+            subMenues: [
+                { path: 'BootstrapInputsExamples', displayName: 'Inputs Examples', hasDivider: true, breadCrumPath: 'Bootstrap Examples/Input Examples' },
+                {
+                    path: 'Login',
+                    displayName: 'Login',
+                    breadCrumPath: 'Bootstrap Examples/Login',
+                },
+                { path: '', displayName: 'Register', breadCrumPath: 'Bootstrap Examples/Register' },
+            ],
         },
     ];
+
+    public breadCrumbPath = [];
 
     constructor() {}
 
     ngOnInit() {}
+
+    public setBreadCrumb(menu: MenuItem) {
+        const path = menu.breadCrumPath.split('/');
+        this.breadCrumbPath = [];
+        const menuFlatten = this.flattenMenu();
+        for (const item of path) {
+            const menuItem = _.findWhere(menuFlatten, { displayName: item });
+            this.breadCrumbPath.push({ displayName: item, path: menuItem.path });
+        }
+
+        console.log(this.breadCrumbPath);
+    }
+
+    private flattenMenu() {
+        let result = [];
+        for (const menu of this.menues) {
+            result.push(menu);
+            result = result.concat(menu.subMenues);
+        }
+
+        return result;
+    }
 }
